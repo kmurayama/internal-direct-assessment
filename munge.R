6dfecad0b74a1d0b02b90e49a4a21b0ad8a6976a
Kentaro Murayama <1821428+kmurayama@users.noreply.github.com>
2020-06-29 17:12:11 -0400

    Initializes a new note with set up in externalized codes
    Somehow read_chunk() failed to show inline preview even with sample codes. Investigate separately.

---
+++ munge.R
@@ -0,0 +1,74 @@
+## ---- clean-assessment} ----
+mdf$`Assessment Name` <- str_to_title(mdf$`Assessment Name`)
+mdf$Name <- str_to_title(str_remove(mdf$Name, '^"'))
+
+pttrn.bus345 <- c("^Final Paper$" = "Ethics Final Paper")
+pttrn.bus499 <-
+  c("(^Final Company Paper$)|(^Company Profile$)|(^Final Company Profile$)|(^Course Profile$)" = "Company Profile (Final Paper)",
+     "Simulation After-Action Report \\(Also Submitted To Gen Ed For Evidence Of Problem Solving\\)" = "Goventure Aar")
+#    "^Development Plan$" = "Reflection And Development Plan")
+# This affects MGT 300 as well.
+pttrn.eco201.a <- c("International Trade Signature Assignment" = "Trade Policy Paper")
+pttrn.eco201.n <- c("^Arguments For Protection$"="Arguments For Protectionism",
+                    "^Citations$"="Citations/References",
+                    "^Structure$"="Structure/Clarity",
+                    "^Winners/Losers$"="Winners And Losers",
+                    "^Writing: Citations$"="Writing: Citations/References")
+pttrn.eco202.a <- c("^Cross-Country Gdp Comparison Paper$" = "Gdp Comparison Paper")
+pttrn.eco202.n <- 
+  c("^Data Collection$" = "Part 1: Data Collection",
+    "^Time-Series Graphs$" = "Part 1: Time Series Plots",
+    "^Explain Variable Movements$" = "Part 2: Explain Variable Movements",
+    "^Short-Run And Long-Run Perspectives$" = "Part 2: Short-Run And Long-Run Perspectives",
+    "^Compare And Contrast$" = "Part 2: Compare And Contrast",
+    "^Citations$" = "Writing: Citations",
+    "^Grammar$" = "Writing: Grammar",
+    "^Structure$" = "Writing: Structure")
+pttrn.mgt300 <- c("^Burt’s Bees (This Is A Re-Submission–I Typed The Course Number Wrong Before)$" = "Burt’s Bees",
+                  "^Burts Bees Case Study$" = "Burt’s Bees")
+
+pttrn.assess <- c(pttrn.bus345, pttrn.bus499, pttrn.eco202.a, pttrn.eco201.a, pttrn.mgt300)
+pttrn.name <- c(pttrn.eco202.n, pttrn.eco201.n)
+
+mdf <- mdf %>%
+  mutate(Name = str_remove(Name, '^"'),
+         `Assessment Name` = str_replace_all(`Assessment Name`, pttrn.assess),
+         Name = str_replace_all(Name, pttrn.name))
+
+### ---- standardize-achievement ----
+mdf$LevelAchieved <- str_replace_all(mdf$LevelAchieved, fixed("Unsatisactory (<60%)"), "Unsatisfactory") # Fix a typo
+
+achievements1 <- c("Unsatisfactory", "Developing", "Basic", "Proficient", "Advanced")
+achievements2 <- c("Unacceptable", "Rudimentary", "Fair", "Good", "Excellent")
+names(achievements2) <- achievements1 # Named list as dictionary
+pttrn <- paste(c(achievements1, achievements2), collapse = "|")
+
+x <- levels(mdf$LevelAchieved)
+
+mdf <- mdf %>% mutate(
+  LevelAchieved.Original = LevelAchieved,
+  LevelAchieved = str_extract(mdf$LevelAchieved, pttrn))
+mdf$LevelAchieved <- str_replace_all(mdf$LevelAchieved, achievements2)
+mdf$LevelAchieved <- ordered(mdf$LevelAchieved, levels = achievements2)
+
+summary(mdf$LevelAchieved)
+
+## ---- new-variables ----
+mdf <- mdf %>% mutate(
+  Met.UND = case_when(
+    is.na(LevelAchieved) ~ "Missing",
+    LevelAchieved == "Excellent" | LevelAchieved == "Good" |
+      LevelAchieved == "Fair" ~ "Met",
+    LevelAchieved == "Rudimentary" | LevelAchieved == "Unacceptable" ~ "Not",
+    TRUE ~ "Other"),
+  Met.GRD = case_when(
+    is.na(LevelAchieved) ~ "Missing",
+    LevelAchieved == "Excellent" | LevelAchieved == "Good" ~ "Met",
+    LevelAchieved == "Fair" | LevelAchieved == "Rudimentary" |
+      LevelAchieved == "Unacceptable" ~ "Not",
+    TRUE ~ "Other"),
+  Online = grepl("GW", Section),
+  Met.UND.bin = Met.UND == "Met",
+  Met.GRD.bin = Met.GRD == "Met"
+  )
+table(mdf$LevelAchieved, mdf$Met.UND, useNA = "ifany")
---
+++ read.R
@@ -0,0 +1,100 @@
+# read.R
+# Read data files, clean them up, and merge them for the analysis.
+# This is the shared component for notebooks (note_inspecting_rubric_data##.Rmd)
+
+
+library(tidyverse)
+library(readxl)
+
+# ---- test-a ----
+1 + 1
+x = rnorm(10)
+
+## ---- import-d2l ----
+fn <- "data/Rubrics.xlsx"
+df <- read_xlsx(fn) %>%
+  mutate(across(c(RubricId:Name, LevelAchieved), factor),
+         IsScoreOverridden = (IsScoreOverridden == "True"))
+head(df)
+
+## ---- import-survey ----
+fn <- "data/Tracking of Assessment of Student Learning Outcomes Data Collection.xlsx"
+res <- read_excel(fn, sheet = "Response0602")
+res <- res[res$`I graded this assessment using a rubric on D2L.` == "Yes", ]
+res <- res[1:12]
+
+qs <- names(res)
+responder.info <- c("ID", "Started", "Completed", "Email", "Name")
+course.info <- c("Semester", "Course")
+assessment.info <- c("Assessment Name", "Rubric Usage")
+rubric.info <- c("URL", "Intrepretation", "Action for Improvement")
+names(res) <- c(responder.info, course.info, assessment.info, rubric.info)
+
+## ---- drop-entries ----
+res <- res[res$ID != 1 & res$ID != 2, ] # Drop early error entries
+
+assess.name <- res[res$ID == 41, c("Assessment Name", "URL")] # Keep double entry 
+anames <- str_split(assess.name$`Assessment Name`, ",", simplify = TRUE)
+urls <- str_split(assess.name$URL, "Burts", simplify = TRUE)
+
+x <- res[res$ID == 41, ]
+x$ID <- nrow(res)
+x[c("Assessment Name", "URL")] <- list(anames[2], urls[2])
+res[res$ID == 41, c("Assessment Name", "URL")] <- list(anames[1], urls[1])
+res[res$ID == 159, ] <- x # Replace Ed's entry with the modified Neeley's
+
+## ---- clean-course ----
+x <- res$Course
+x <- str_replace(x, "GW[I|!]", "GW1") # Replace some typos
+x <- str_replace(x, "GS1", "GW1") # Replace some typos
+x <- str_remove(x, "[0-9]{4,}") # Remove leading time stamp
+x <- str_replace(x, "BS[U]?", "BUS") # Replace less critical typo
+x <- str_replace(x, "-01", "-001") # Replace less critical typo
+
+cid1 <- str_match(x, "[A-Z]{2,3}") # Extract course program (e.g. BUS, MKT)
+cid2 <- str_extract_all(x, "(CD|G[WS])?[0-9]{1,3}", simplify = TRUE) # Extract
+res$Course <- paste(cid1, cid2[, 1]) # Then merge for course id e.g. MGT 300
+res$Section <- paste(cid1, cid2[, 1], cid2[, 2]) # Likewise, e.g. MGT 300 001
+
+## ---- extract-rubricid ----
+x <- str_extract_all(res$URL, "rubricId=[0-9]{6}", simplify=TRUE)
+res$RubricId <- factor(str_extract_all(x, "[0-9]{6}", simplify=TRUE))
+
+#tbl <- table(res$RubricId)
+#dups <- tbl[tbl > 1]
+#res[res$RubricId==names(dups)[1], ]
+## Drop 2, 22
+#res[res$RubricId==names(dups)[2], ]
+## Drop 41 (see the note)
+#res[res$RubricId==names(dups)[3], ]
+## Drop 77 Why resubmit?
+#res[res$RubricId==names(dups)[4], ]
+## Drop 71 ?
+#res[res$RubricId==names(dups)[5], ]
+## Drop 1
+#res[res$RubricId==names(dups)[6], ]
+## Drop 83 ?
+#res[res$RubricId==names(dups)[7], ]
+## Drop 88 ?
+#res[res$RubricId==names(dups)[8], ]
+## Different sections but provided same ID. Actual error. Drop 116.
+#res[res$RubricId==names(dups)[9], ]
+## Drop 37
+#res[res$RubricId==names(dups)[10], ]
+## Drop 93 ?
+#res[res$RubricId==names(dups)[11], ]
+## Drop 92 ?
+
+id.drop <- c(2, 22, 41, 77, 71, 1, 83, 88, 116, 37, 93, 92)
+res <- res %>% filter(!ID %in% id.drop)
+
+res.trim <- res %>%
+  mutate(across(c(ID, Email:Semester, Section:Course), factor) )
+
+## ---- merge ----
+dim(df)
+mdf <- df %>%
+  left_join(res.trim %>% select(RubricId, Instructor = Name, Course,
+                                `Assessment Name`, Semester,
+                                Section, Course), by="RubricId")
+dim(mdf)
